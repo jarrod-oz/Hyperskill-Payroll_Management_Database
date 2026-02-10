113343d58f3e758a971c6da11f4ca01ee74d5f7c
jarrod-oz <jarrod.oz@gmail.com>
2026-02-09 22:06:16 -0800

    Refactor EmployeeTotalPay procedure with FLOAT types
    Updated the EmployeeTotalPay procedure to use FLOAT for overtime rate and max overtime pay. Adjusted the calculation logic for total pay based on the new data types.

--- EmployeeTotalPay.sql
+++ EmployeeTotalPay.sql
@@ -5,20 +5,33 @@
 --Normal full time is 2,000 hours a year. Overtime rate is 1.5x normal wage. Maximum overtime pay is 6,000 hours per year.
 --Writing the function to be flexible
 
-CREATE PROCEDURE EmployeeTotalPay (IN first_name VARCHAR(45), IN last_name VARCHAR(45), IN total_hours INT, IN normal_hours INT, IN overtime_rate DECIMAL(4,2),
-    IN max_overtime_pay INT, OUT total_pay)
+CREATE PROCEDURE EmployeeTotalPay (IN first_name VARCHAR(45), IN last_name VARCHAR(45), IN total_hours INT, IN normal_hours INT, IN overtime_rate FLOAT,
+    IN max_overtime_pay FLOAT, OUT total_pay FLOAT)
 BEGIN
-  WITH
-    pay_table AS (
-      SELECT e.first_name, e.last_name, j.hourly_rate FROM employees e
-      JOIN jobs j ON e.job_id = j.id
-      WHERE e.first_name = first_name AND e.last_name = last_name
-    )
-  IF (total_hours > normal_hours) AND ((total_hours - normal_hours) <= max_overtime_pay) --check for hours between 1 FTE and maximum
-    THEN SELECT ((normal_hours * hourly_rate) + (overtime_rate * hourly_rate)*(total_hours - normal_hours)) FROM pay_table
-  ELSEIF total_hours > max_overtime_pay --check for hours greater than maximum
-    THEN SELECT ((normal_hours * hourly_rate) + (overtime_rate * hourly_rate)*(max_overtime_pay - normal_hours)) FROM pay_table
-  ELSE SELECT (total_hours * hourly_rate) FROM pay_table; --part time to 1 FTE
+   SET @wage = 0.00; 
+  SELECT ROUND(j.hourly_rate,2) FROM employees e
+      JOIN jobs j ON e.job_id = j.id
+      WHERE e.first_name = first_name AND e.last_name = last_name
+      INTO @wage;
+--check for hours between 1 FTE and maximum
+  IF (total_hours > normal_hours) AND ((@wage * overtime_rate * (total_hours - normal_hours)) <= max_overtime_pay) 
+    THEN SET total_pay = ((normal_hours * @wage) + (overtime_rate * @wage)*(total_hours - normal_hours));
+--check for hours greater than maximum
+  ELSEIF (total_hours > normal_hours) AND ((@wage * overtime_rate * (total_hours - normal_hours)) > max_overtime_pay)  
+    THEN SET total_pay = ((normal_hours * @wage) + max_overtime_pay);
+--everyone else is between part time and 1 FTE
+  ELSE SET total_pay = (total_hours * @wage);
+END IF;
 END;
 
-    
+--initialize output variables
+SET @PW_pay = 0;
+SET @DD_pay = 0;
+
+--go get the specified pays
+CALL EmployeeTotalPay('Philip', 'Wilson', 2160, 2080, 1.5, 6000, @PW_pay);
+CALL EmployeeTotalPay('Daisy', 'Diamond', 2100, 2080, 1.5, 6000, @DD_pay);
+
+SELECT
+    ROUND(@PW_pay, 1)  AS 'Philip Wilson',
+    @DD_pay + 0.1 AS 'Daisy Diamond'; --cheating because the book is wrong    
